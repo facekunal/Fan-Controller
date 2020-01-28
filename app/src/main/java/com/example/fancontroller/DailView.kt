50645f3eae9a127b0267d9cf516142f615b52ab5
facekunal <facekunal@gmail.com>
2020-01-28 22:44:25 +0530

    init project

---
+++ app/src/main/java/com/example/fancontroller/DailView.kt
@@ -0,0 +1,14 @@
+package com.example.fancontroller
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+
+class DailView @JvmOverloads constructor(
+    context: Context,
+    attr: AttributeSet,
+    defStyleAttributes: Int
+): View(context, attr, defStyleAttributes) {
+
+}
